003c5c24aca58a0d9cfcb2d9c93880e0bae7727d
AbhayMaheshwari123 <43092362+AbhayMaheshwari123@users.noreply.github.com>
2021-09-10 14:13:56 +0530

    Implementation of useState with previous state

--- Hook/App.js
+++ Hook/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HookCounter from './Component/HookCounter';
+import HookCounter2 from './Component/HookCounter2';
 
 function App() {
   return (
     <div className="App">
       <HookCounter></HookCounter>
+      <HookCounter2></HookCounter2>
     </div>
   );
 }
