c2562d089680e7444a1890ce53206b81ab2857cb
AbhayMaheshwari123 <43092362+AbhayMaheshwari123@users.noreply.github.com>
2021-09-10 15:24:46 +0530

    Implementation of useState with object

--- Hook/App.js
+++ Hook/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HookCounter from './Component/HookCounter';
 import HookCounter2 from './Component/HookCounter2';
+import HookCounter3 from './Component/HookCounter3';
 
 function App() {
   return (
     <div className="App">
       <HookCounter></HookCounter>
       <HookCounter2></HookCounter2>
+      <HookCounter3></HookCounter3>
     </div>
   );
 }
