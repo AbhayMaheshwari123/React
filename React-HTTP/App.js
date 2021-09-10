1eb90683db9f3623621a21aeaa0ef808dc046d7d
AbhayMaheshwari123 <43092362+AbhayMaheshwari123@users.noreply.github.com>
2021-09-10 12:00:23 +0530

    Add files via upload

---
+++ React-HTTP/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GetReqst from './Components/GetReqst';
+
+function App() {
+  return (
+    <div className="App">
+     <GetReqst></GetReqst>
+    </div>
+  );
+}
+
+export default App;
