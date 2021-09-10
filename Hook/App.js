fa7ce8d9461666c2205173729601a10db7b33698
AbhayMaheshwari123 <43092362+AbhayMaheshwari123@users.noreply.github.com>
2021-09-10 15:31:16 +0530

    Implementation of useState with array

--- Hook/App.js
+++ Hook/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HookCounter from './Component/HookCounter';
 import HookCounter2 from './Component/HookCounter2';
 import HookCounter3 from './Component/HookCounter3';
+import HookCounter4 from './Component/HookCounter4';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <HookCounter></HookCounter>
       <HookCounter2></HookCounter2>
       <HookCounter3></HookCounter3>
+      <HookCounter4></HookCounter4>
     </div>
   );
 }
